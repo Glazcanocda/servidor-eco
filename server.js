0f135ff4104d0be43fbff3597ccbe4a4e0db72b4
Glazcanocda <164578442+Glazcanocda@users.noreply.github.com>
2026-09-10 10:49:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
+app.enable('trust proxy');
+
 const PORT = process.env.PORT || 9000;
 
-// Ruta principal para confirmación de estado de Render
 app.get('/', (req, res) => {
-    res.send('Servidor de Señalización Ecográfica Activo y Funcionando');
+    res.send('Servidor de Señalización Ecográfica Activo');
 });
 
-// Inicializar el servidor HTTP
 const server = app.listen(PORT, () => {
-    console.log(`Servidor iniciado y escuchando en el puerto ${PORT}`);
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-// Adjuntar el servidor PeerJS a Express
 const peerServer = ExpressPeerServer(server, {
     debug: true,
-    path: '/'
+    path: '/',
+    proxied: true
 });
 
 app.use('/peerjs', peerServer);
